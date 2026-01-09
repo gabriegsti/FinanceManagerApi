c8c32f4d9249e510b9cd421dd6212f9e44e99b98
gabriegsti <gabrielgsdevbr@gmail.com>
2026-01-09 18:08:39 -0300

    Assets and Currency first commit

---
+++ FinanceManagerApi/Entities/Asset.cs
@@ -0,0 +1,38 @@
+﻿namespace FinanceManagerApi.Entities
+{
+    public class Asset
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public decimal Value { get; set; }
+        public Currency Currency { get; set; }
+
+        public Asset(string name, string code, string description, decimal value, Currency currency)
+        {
+            if(String.IsNullOrEmpty(name))
+                throw new ArgumentNullException("name");
+            if(String.IsNullOrEmpty(code)) 
+                throw new ArgumentNullException("code");
+            if(value < 0) 
+                throw new ArgumentOutOfRangeException("value cannot be below zero");
+
+            Name = name;
+            Code = code;
+            Description = description;
+            Value = value;
+            Currency = currency;
+        }
+
+        public void UpdateValue(decimal newValue, Currency targetCurrency)
+        {
+            if(newValue >= 0)
+            {
+                Value = Currency.ConvertTo(newValue, targetCurrency);
+                Currency = targetCurrency;
+            }
+            throw new ArgumentException("Value cannot be below zero");
+        }
+    }
+}
---
+++ FinanceManagerApi/Entities/Currency.cs
@@ -0,0 +1,29 @@
+﻿namespace FinanceManagerApi.Entities
+{
+    public class Currency
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal ExchangeRateInUSDollar { get; set; }
+
+        public Currency(string name, decimal exchangeRateInUSDollar)
+        {
+            if (String.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("name");
+            }else if (ExchangeRateInUSDollar < 0)
+            {
+                throw new ArgumentException("ExchangeRateInUSDollar lower than zero");
+            }
+
+            Id = Guid.NewGuid();
+            Name = name;
+            ExchangeRateInUSDollar = exchangeRateInUSDollar;
+        }
+
+        public decimal ConvertTo(decimal amount, Currency targetCurrency) {
+            var amountInUSD = amount * ExchangeRateInUSDollar; 
+            return amountInUSD / targetCurrency.ExchangeRateInUSDollar;
+        }
+    }
+}
